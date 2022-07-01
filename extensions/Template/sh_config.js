7eeadc8afbc29b3deaf143c5d2495a77c15cba66
ItsAmmarB <ItsAmmarB@outlook.com>
2022-07-01 12:31:10 +0300

    Extensions Template

---
+++ extensions/Template/sh_config.js
@@ -0,0 +1,16 @@
+SH_Config.Extensions.push({
+    name: 'Template', // Extension name here, and make sure it the same as the one in the extension class in the 'sv_index.js'
+    config: {
+
+    }
+});
+
+/**
+ * This is a shared config file between the server and the client
+ * you can add variables here that can be shared between both server and the client
+ * make sure to not put anything sensitve here like token, api keys, etc..
+ *
+ * this can be used to pass variables between client and server, have configurable variables that are used on both client and server
+ *
+ * this file can be removed if not needed, it was created to let people know that it can be used
+ */
