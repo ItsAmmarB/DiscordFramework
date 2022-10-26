7c5865d10870b3724c8f22c194109a5a57f72bfa
ItsAmmarB <ItsAmmarB@outlook.com>
2022-10-26 13:41:31 +0300

    updated string.prototype.format

--- Core/Utils/prototypes.js
+++ Core/Utils/prototypes.js
@@ -1,11 +1,8 @@
 // Credit: https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
-if (!String.prototype.format) {
-    String.prototype.format = function format() {
-        const args = arguments[0];
-        let text = this;
-        for (const elm in args) {
-            text = text.replace(`{${elm}}`, match => args[elm] != 'undefined' ? args[elm] : match);
-        }
-        return text;
-    };
-}
+String.prototype.format = function() {
+    let formatted = this;
+    for (const arg in arguments) {
+        formatted = formatted.replace('{' + arg + '}', arguments[arg]);
+    }
+    return formatted;
+};
