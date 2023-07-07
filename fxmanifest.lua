58d5ec6671a35b62e7aa76af21075dd8d2db355e
Ammar B <13232108+ItsAmmarB@users.noreply.github.com>
2023-07-07 11:46:20 +0300

    added extesions files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,5 +4,25 @@ game 'gta5'
 author 'ItsAmmarB'
 version '0.0.3'
 
-client_script 'cfx/client.js'
-server_script 'cfx/server.js'
+debug_mode 'true'
+
+shared_scripts {
+    'cfx/shared.js',
+
+    -- Extensions' files
+    'src/extensions/**/sh_*.js'
+}
+
+client_scripts {
+    'cfx/client.js',
+
+    -- Extensions' files
+    'src/extensions/**/cl_*.js'
+}
+
+server_scripts {
+    'cfx/server.js',
+
+    -- Extensions' files
+    'src/extensions/**/sv_*.js'
+}
