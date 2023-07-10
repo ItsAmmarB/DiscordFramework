e413eb6468374761c721cfd24313ce39c4307756
Ammar B <13232108+ItsAmmarB@users.noreply.github.com>
2023-07-10 13:23:16 +0300

    only onConnection allow as utility for extensions

--- src/utilities.js
+++ src/utilities.js
@@ -1,9 +1,11 @@
 const Players = require('./components/player');
-const Connections = require('./index');
+const onConnection = require('./index').onConnection;
 const Console = require('./components/console');
 
 module.exports = {
     Players,
-    Connections,
+    Connections: {
+        onConnection
+    },
     Console
 };
