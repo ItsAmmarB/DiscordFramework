c55bec5905d05c0ef2c462f4d6cb29a783352ffa
Ammar B <13232108+ItsAmmarB@users.noreply.github.com>
2023-07-10 10:55:14 +0300

    Switch console.log with globa.Debug on cfx exports

--- CFX/server.js
+++ CFX/server.js
@@ -2,7 +2,7 @@
  * This registers exports without changing the environment behavior of the calling file
  */
 on('DiscordFramework:Export:Create', (Name, Function) => {
-    console.log(Name);
+    Debug(`Exporting Exports.${Name}`);
     exports(Name, Function);
 });
 
