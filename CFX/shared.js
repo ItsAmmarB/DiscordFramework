585b3ad66054e6020b10e68c3ee1160c116324c5
Ammar B <13232108+ItsAmmarB@users.noreply.github.com>
2023-07-10 10:06:50 +0300

    globalized Debug()

--- CFX/shared.js
+++ CFX/shared.js
@@ -10,7 +10,7 @@ global.Delay = async MS => await new Promise(resolve => setTimeout(resolve, MS))
  * Send a labeled log to the console if debug mode is enabled
  * @param {string} msg The message to send to the console if debug mode is enabled
  */
-const Debug = msg => {
+global.Debug = msg => {
     const isDebugEnabled = String(GetResourceMetadata(GetCurrentResourceName(), 'debug_mode', 0)).toLowerCase() === 'true' ? true : false;
     if(isDebugEnabled) console.log(`^5[DEBUG] ^3${msg.split('\n').join('\n        ')}^0`);
 };
