44bc5fa20ea51886846dd98dd63517f7a0415744
Ammar B <13232108+ItsAmmarB@users.noreply.github.com>
2023-07-12 01:27:34 +0300

    split global.Debug to client & server

--- CFX/shared.js
+++ CFX/shared.js
@@ -5,13 +5,3 @@
  * @returns {Promise<void>}
  */
 global.Delay = async MS => await new Promise(resolve => setTimeout(resolve, MS));
-
-/**
- * Send a labeled log to the console if debug mode is enabled
- * @param {string} msg The message to send to the console if debug mode is enabled
- */
-global.Debug = msg => {
-    const isDebugEnabled = String(GetResourceMetadata(GetCurrentResourceName(), 'debug_mode', 0)).toLowerCase() === 'true' ? true : false;
-    if(!msg) return isDebugEnabled;
-    if(isDebugEnabled) console.log(`^5[DEBUG] ^3${msg.split('\n').join('\n        ')}^0`);
-};
